0fa5a5ce448f5d54744399968205721a3bfa5d7d
Antonio Cristovam <84741807+antoniocristovam@users.noreply.github.com>
2022-02-19 18:25:23 -0300

    Delete provacap1.js

--- Prova/provacap1.js
+++
@@ -1,9 +0,0 @@
-let degrais = '';
-let valorReceber = 6; // Quantidade de degrais
-
-for (numero = 1; numero <= valorReceber; numero++) {
-    degrais = ' '.repeat(valorReceber - numero) + '*'.repeat(numero);
-    console.log(degrais);
-};
-
-
